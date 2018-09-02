44eedecc5a19c975760cf65f6dfb6295035e1a22
marcuxo <marcourrutiam@gmail.com>
2018-09-01 23:06:47 -0300

    Add Autor MarcoUrrutiaMolina

--- ClubSocialST/AddData.cs
+++ ClubSocialST/AddData.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/**
+ * @autor MarcoUrrutiaMolina
+ */
 namespace ClubSocialST
 {
     class AddData
